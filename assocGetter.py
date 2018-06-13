d3d0598a5bc4e3e3f12a0ae1bd7c7a1e7b00d4a0
binyuman <ben@vicarius.io>
2018-06-13 14:22:31 +0300

    changed description of dicgetter. also added assocGetter - new scraper that gets common association of words - for clue making !

---
+++ assocGetter.py
@@ -0,0 +1,68 @@
+#BAKER
+import urllib2
+from HTMLParser import HTMLParser
+
+class myParser(HTMLParser) : #customizing Python's built in parser, it essentially just adds all suggestions to list
+
+
+    def __init__(self) :
+        HTMLParser.__init__(self)
+        self.words = []
+        self.inSection = False
+        self.inLink = False
+
+    def handle_starttag(self, tag, attrs):
+
+        if tag == "div" :
+
+            self.inLink = False
+
+            for key, value in attrs :
+
+                if key == "class" and value == "section NOUN-SECTION" :
+
+                    self.inSection = True
+
+        elif tag == "a" :
+
+            self.inLink = True
+
+        else :
+
+            self.inLink = False
+
+    def handle_data(self, data) :
+
+        if self.inSection and self.inLink :
+
+            self.words.append(data)
+
+
+    def handle_endtag(self, tag) :
+
+        if tag == "div" and self.inSection :
+
+            self.inSection = False
+
+
+
+
+def requestMaker(word) :
+
+
+
+    url = "https://wordassociations.net/en/words-associated-with/" + word
+
+    req = urllib2.Request(url, None)
+    html = urllib2.urlopen(req).read()
+    return html
+
+
+def use(word) : #WORKS - GENERATES COMMON ASSOCIATIONS WITH WORD - HELPFUL FOR CLUE MAKING !!!!
+
+    parser = myParser()
+    parser.feed(requestMaker(word))
+    print parser.words
+
+#USAGE :
+use("pig")
