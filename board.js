177057309d3adffd9ff4a9f93a8b1500c2a07dd3
Bayless <baylemichal@gmail.com>
2018-06-25 21:23:08 -0400

    separated css and js on homepage

--- board.js
+++ board.js
@@ -4,7 +4,6 @@ function createBoard(numRows) {
     var oldBoard = document.getElementById("board");
     var newTable = document.createElement("TABLE");
     if (oldBoard) {
-	//alert("table exists");
 	parent.replaceChild(newTable,oldBoard);
 	newTable.setAttribute("id","board");
 	var board = newTable;
---
+++ homepage.js
@@ -0,0 +1,21 @@
+// Get the modal
+var modal = document.getElementById('login');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+document.getElementById("labels").style.margin = "30px";
+document.getElementById("greyBar").style.margin = "30px";	// Get the modal
+var modal = document.getElementById('register');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+document.getElementById("labels").style.margin = "30px";
+document.getElementById("greyBar").style.margin = "30px";
