f106378cbaa7ef99848c3bd81a1f3a5a58501ce6
Bayless <baylemichal@gmail.com>
2018-07-02 18:49:34 -0400

    can run locally now but login still doesnt work

--- back.py
+++ back.py
@@ -1,6 +1,6 @@
 #BAKER
 
-from flask import Flask, Response
+from flask import Flask, Response, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
@@ -16,14 +16,57 @@ def start() :
     else:
         return render_template("homepage.html")
 
- #   response = open("homepage.html")
- #   return Response(response, mimetype='text/plain')
+@app.route("/")
+def loginOrRegister():
+    if 'username' in session:
+        return redirect("/puzzle")
+    else:
+        return render_template("homepage.html")
+
+@app.route("/authOrCreate", methods=["POST"])
+def authOrCreate():
+    formDict = request.form
+    if formDict["logOrReg"] == "login":
+        username = formDict["username"]
+        password = formDict["password"]
+        loginStatus = "login failed"
+        statusNum = accountManager.authenticate(username,password) #returns 0,1 or 2 for login status messate
+        if statusNum == 0:
+            loginStatus = "user does not exist"
+        elif statusNum == 1:
+            session["username"]=username
+            loginStatus = username + " logged in"
+            return render_template("board.html")
+        elif statusNum == 2:
+            loginStatus = "wrong password"
+        return render_template("homepage.html") #,status=loginStatus)
+
+    elif formDict["logOrReg"] == "register":
+        username = formDict["username"]
+        password = formDict["password"]
+        pwd = formDict["pwd"]  #confirm password
+        registerStatus = "register failed"
+        statusNum = accountManager.register(username,password,pwd) #returns true or false
+        if statusNum == 0:
+            registerStatus = "username taken"
+        elif statusNum == 1:
+            registerStatus = "passwords do not match"
+        elif statusNum == 2:
+            registerStatus = username +" account created"
+        elif statusNum == 3:
+            registerStatus = "password too short"
+        elif statusNum == 4:
+            registerStatus = "username left blank"
+        return redirect("/") #render_template("homepage.html") #,status = registerStatus) #status is the login/creation messate
+    else:
+        return redirect(url_for("loginOrRegister"))
+
 
 @app.route("/puzzle")
 def crossword() :
      if 'username' not in session:
         return redirect("/")
-    else:
+     else:
         return render_template("board.html")
 
 
@@ -33,5 +76,10 @@ def logout():
     if "username" in session:
         session.pop('username')
     return redirect(url_for('/'))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
 
